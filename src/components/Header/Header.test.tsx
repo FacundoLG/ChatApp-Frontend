882aecfb8dc209c28017691bfbad11197d58bec4
Facundo Gimenez <facu-judoca@hotmail.com.ar>
2022-06-08 11:14:40 -0300

    header component

---
+++ src/components/Header/Header.test.tsx
@@ -0,0 +1,7 @@
+import Header from './Header'
+
+describe('Tests for Header', () => {
+  test('Header should render', () => {
+    expect(1).toBe(1)
+  })
+})
--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,6 +1,24 @@
-function Header () {
+import styles from './header.module.css'
+import LOGO from '../../assets/svgs/Logo.svg'
+import { FC } from 'react'
+import { AiFillSetting, AiFillBell } from 'react-icons/ai'
+interface HeaderProps {
+  username?: string
+  notifications_length?: number
+}
+
+const Header:FC<HeaderProps> = ({ username }) => {
   return (
-    <div>Hea asd asd asder</div>
+    <header className={styles.header}>
+      <div>
+        <img className={styles.logo} src={LOGO} alt="Chat app logo" />
+        <p>{username}</p>
+      </div>
+      <nav className={styles.nav}>
+        <a href=""> <AiFillBell/> </a>
+        <a href=""> <AiFillSetting/> </a>
+      </nav>
+    </header>
   )
 }
 

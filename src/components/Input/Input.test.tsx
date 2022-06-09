b5c7fcc2211379ec75fab0ec9de4726354abfd87
Facundo Gimenez <facu-judoca@hotmail.com.ar>
2022-06-09 12:46:43 -0300

    test

--- src/components/Input/Input.test.tsx
+++ src/components/Input/Input.test.tsx
@@ -1,1 +1,16 @@
+import { render, screen } from '@testing-library/react'
 import Input from './Input'
+
+describe('Input tests', () => {
+  test('Inner input exist and contain the provided placeholder and value', () => {
+    render(<Input placeholder='place' value='value'/>)
+    const element = screen.getByPlaceholderText('place') as HTMLInputElement
+    expect(element).toBeInTheDocument()
+    expect(element.value).toBe('value')
+  })
+  test('Error message should exist', () => {
+    render(<Input error='Hola manola' />)
+    const errorTag = screen.getByText('Hola manola')
+    expect(errorTag).toBeInTheDocument()
+  })
+})
